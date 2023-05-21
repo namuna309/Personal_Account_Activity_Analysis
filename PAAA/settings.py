d6e200fde2242b6410efc546c0a5cc2ef27569c7
Nam Yoonah <nyah309@gmail.com>
2023-05-21 11:22:47 +0900

    feat/aws

--- PAAA/settings.py
+++ PAAA/settings.py
@@ -80,8 +80,17 @@ WSGI_APPLICATION = 'PAAA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PAAA',
+        'USER': 'admin',
+        'PASSWORD': 'arya180317!',
+        'HOST': 'paaa-db.cxogtdbxdeaw.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        }
     }
 }
 
