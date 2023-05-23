553d133fe55a6905dc3ec73dd3fef74e0b721a86
Nam Yoonah <nyah309@gmail.com>
2023-05-24 08:29:13 +0900

    local/hotfeax_crawldata

--- apps/explore_tab_crawling/views.py
+++ apps/explore_tab_crawling/views.py
@@ -3,50 +3,55 @@ from apps.explore_tab_crawling.crawler.explore_tab import InstagramCrawler
 from .models import *
 # Create your views here.
 
+
 def save_contents():
     username = "namuna_crawl"
-    password = "dbsdk309!"    
+    password = "dbsdk309!"
 
-    # 인스타그램 크롤러 인스턴스 생성
-    crawler = InstagramCrawler(username, password)
-    
-    # 로그인
-    crawler.login()
-    
-    # 데이터 크롤링
-    likes, comments, hashtags, datetimes, slide, urls = crawler.crawl_data()
+    while True:
+        # 인스타그램 크롤러 인스턴스 생성
+        crawler = InstagramCrawler(username, password)
 
-    crawler.quit()
+        # 로그인
+        crawler.login()
+
+        # 데이터 크롤링
+        likes, comments, hashtags, datetimes, slide, urls = crawler.crawl_data()
+        if len(likes) == 0 and len(comments) == 0 and len(hashtags) == 0 and len(datetimes) == 0 and len(slide) == 0 and len(urls) == 0:
+            crawler.quit()
+            continue
+        else:
+            crawler.quit()
+            break
 
     for i in range(len(likes)):
         content = Content(
-            hashtags = hashtags[i],
-            likes = likes[i],
-            comments = comments[i],
-            slide = slide[i],
-            url = urls[i],
-            created_at = datetimes[i]
+            hashtags=hashtags[i],
+            likes=likes[i],
+            comments=comments[i],
+            slide=slide[i],
+            url=urls[i],
+            created_at=datetimes[i]
         )
 
         content.save()
 
-def save_hashtags() :
+
+def save_hashtags():
     contents = Content.objects.all()
 
-    for content in contents :
+    for content in contents:
         hashtags = content.hashtags.split(' ')
         for hash in hashtags:
-            if hash == '' :
+            if hash == '':
                 continue
-            else :
-                try :
+            else:
+                try:
                     hashtag = Hashtag(
-                        content_id = content,
-                        text = hash,
-                        frequency = 1
+                        content_id=content,
+                        text=hash,
+                        frequency=1
                     )
                     hashtag.save()
-                except Exception as e :
+                except Exception as e:
                     continue
-
-        
